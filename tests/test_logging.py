f8175e95dbc0840c5d877f6c2d261e4345657f95
cliffmu <113331907+cliffmu@users.noreply.github.com>
2025-06-14 17:19:24 -0700

    Revert "Improve logging utility robustness"

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,6 +1,6 @@
 import logging
 
-import utils.logging as log
+from utils import logging as log
 
 
 def test_info_log_level(hass, caplog):
@@ -13,16 +13,3 @@ def test_debug_toggle(hass, caplog):
     caplog.set_level(logging.DEBUG)
     log.debug("Verbose")
     assert "Verbose" in caplog.text
-
-
-def test_debug_handles_format_errors(hass, caplog):
-    caplog.set_level(logging.DEBUG)
-    log.debug("Missing placeholder:", {"foo": 1})
-    assert "Missing placeholder:" in caplog.text
-    assert "'foo': 1" in caplog.text
-
-
-def test_debug_wrong_arg_count(hass, caplog):
-    caplog.set_level(logging.DEBUG)
-    log.debug("Two %s %s", "onlyone")
-    assert "Two" in caplog.text
