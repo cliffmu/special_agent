aa42ad647143565be6fd7f7faa1c20dd47e44999
cliffmu <113331907+cliffmu@users.noreply.github.com>
2025-10-04 11:35:12 -0700

    Fix config flow error

--- config_flow.py
+++ config_flow.py
@@ -54,6 +54,10 @@ class SpecialAgentConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 class SpecialAgentOptionsFlow(config_entries.OptionsFlow):
     """Handle options for Special Agent."""
 
+    def __init__(self, config_entry):
+        """Initialize options flow."""
+        self.config_entry = config_entry
+
     async def async_step_init(self, user_input: dict[str, Any] | None = None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
